f41134b69390bd08dc2dea5de8e6f3c8c71565ed
Jonathan Glad <jonathan.martenssonglad@utb.ecutbildning.se>
2022-02-08 16:43:10 +0100

    Rating on answer-poll working

--- frontend/src/QuestionTypes/RatingQuestion.js
+++ frontend/src/QuestionTypes/RatingQuestion.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import AnswerObj from '../blueprints/AnswerClass';
 import { makeId } from '../blueprints/makeId';
@@ -7,48 +7,57 @@ import { ANSWER_QUESTION, UPDATE_POLL } from '../texts';
 //index = question's index; i = options index.
 function RatingQuestion({ q, qIndex }) {
     const dispatch = useDispatch();
+    const answeredQuestions = useSelector(state => state.answeredQuestions)
+    const [rating, setRating] = useState(1);
+    let stars = [1, 2, 3, 4, 5];
+    const starPercentage = (rating / 5) * 100 + '%';
 
-    const radioOnChange = (e, oIndex) => {
+    const onChange = (e, oIndex) => {
 
         const answer = new AnswerObj(q.question, e.target.value, "rating", qIndex);
-        console.log("answer", answer);
 
         dispatch({
             type: ANSWER_QUESTION,
             payload: { ...answer }
         });
-        // console.log("poll", poll)
-        /* 
-        {
-            answer: e.target.value,
-            indexOfQuestion: qIndex,
-            typeOfQuestion: "radio",
-            question: q.question,
-            indexOfPickedOption: oIndex,
-        }
-        */
-        // const updatedPoll = poll.questions[qIndex];
-        // const updatedQuestion = updatedPoll.type.options[oIndex];
-        // dispatch({ type: UPDATE_POLL, payload: { ...state, poll: {...poll, } } });
-        // console.log("updatedQuestion", updatedQuestion);
     }
 
     return (
         <div>
-            <input type={"radio"} name={`${qIndex}`} className='star-rating' value={1} id='1' />
-            <label htmlFor={'1'}> * </label>
+            <div className='star-container'>
+                {/* <div className="star-outer">
+                    <div className="star-inner" style={{ width: starPercentage }}></div>
+                </div> */}
+                <div className='star-outer'>
+                    {
+                        stars.map((r, index) => {
+                            const id = makeId(qIndex, r, index);
 
-            <input type={"radio"} name={`${qIndex}`} className='star-rating' value={2} id='2' />
-            <label htmlFor={'2'}> * </label>
+                            return (
+                                <span key={index} className='star-bg'>
+                                    <input type={"radio"} name={`${qIndex}`} className='star-rating' value={r} id={id} onChange={(e) => {
+                                        setRating(e.target.value)
+                                        onChange(e, index)
+                                    }} />
+                                    <label className='star' htmlFor={id} style={{ color: (r <= rating) ? "#FFA500" : "#c4c4c4" }}> <i className="fas fa-star"></i> </label>
+                                </span>
+                            )
+                        })
+                    }
+                </div>
+                {/* {
+                    stars.map((rating, index) => {
+                        const id = makeId(qIndex, rating, index);
 
-            <input type={"radio"} name={`${qIndex}`} className='star-rating' value={3} id='3' />
-            <label htmlFor={'3'}> * </label>
-
-            <input type={"radio"} name={`${qIndex}`} className='star-rating' value={4} id='4' />
-            <label htmlFor={'4'}> * </label>
-
-            <input type={"radio"} name={`${qIndex}`} className='star-rating' value={5} id='5' />
-            <label htmlFor={'5'}> * </label>
+                        return (
+                            <span key={index}>
+                                <input type={"radio"} name={`${qIndex}`} className='star-rating' value={rating} id={id} />
+                                <label className='star' htmlFor={id}> <i className="fas fa-star"></i> </label>
+                            </span>
+                        )
+                    })
+                } */}
+            </div>
             {
                 /*  q.options.map((o, i) => <div key={i}>
                      <input type={"radio"} value={`${o}`} name={`${qIndex}`} id={makeId(qIndex, o, i)} onChange={(e) => radioOnChange(e, i)} />
